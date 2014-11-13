b9e30f5c697886682c2e21ca69a315ac5f14fd7a
JanO Mnemonix <jmnemonix@sl9.eu>
2014-11-13 12:01:46 +0100

    Dateien

---
+++ bitsonair/src/infrastruktur/Source.java
@@ -0,0 +1,5 @@
+package infrastruktur;
+
+public class Source {
+
+}
---
+++ bitsonair/src/visualisierung/Fenster.java
@@ -0,0 +1,20 @@
+package visualisierung;
+
+import javax.swing.JFrame;
+
+public class Fenster extends JFrame {
+	
+	public Fenster() {
+		super();
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
